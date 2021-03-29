b75f6baf59220fc1e71a128232b31519e5d9dd69
Graciellemtns <gracimtns@gmail.com>
2021-03-29 20:19:04 -0300

    Primeiro commit

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Mel"
+    var cpf: String = "126.251.845-87"
+
+}
+
+fun main() {
+    val Mel = Pessoa()
+
+    println(Mel.nome)
+    println(Mel.cpf)
+}
