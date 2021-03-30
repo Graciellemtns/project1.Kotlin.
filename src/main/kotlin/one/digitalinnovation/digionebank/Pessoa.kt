03ba1683f9f6cc78e6052db503f7fa9548214754
Graciellemtns <gracimtns@gmail.com>
2021-03-30 16:48:55 -0300

    update to apply the private modifer to set function of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,12 +3,17 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Mel"
     var cpf: String = "126.251.845-87"
+    private set // não conseguir alterar dentro da função o cpf e o nome
 
 }
 
 fun main() {
     val Mel = Pessoa()
 
+    //println(Mel)
     println(Mel.nome)
     println(Mel.cpf)
+
+
+
 }
