43322b3da7815aeb38da5ac3d99ad62153c213dc
Graciellemtns <gracimtns@gmail.com>
2021-03-30 20:47:16 -0300

    update open class2

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Pessoa
+
+fun main(){
+    val Mel = Pessoa(nome = "Mel Martins", cpf = "123.456.789.01")
+    println(Mel.nome)
+    println(Mel.cpf)
+}
