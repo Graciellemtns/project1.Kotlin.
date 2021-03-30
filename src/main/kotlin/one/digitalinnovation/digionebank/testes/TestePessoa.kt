d51922f0b6bd8942c74e363360decd1b2e9a6a1f
Graciellemtns <gracimtns@gmail.com>
2021-03-30 20:55:04 -0300

    update to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.digionebank.testes
-
-import one.digitalinnovation.digionebank.Pessoa
-
-fun main(){
-    val Mel = Pessoa(nome = "Mel Martins", cpf = "123.456.789.01")
-    println(Mel.nome)
-    println(Mel.cpf)
-}
